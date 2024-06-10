a7919a574cfe8c9f0ce5f89e58098f932810b7b0
Yamada Kenji <y.kenji@geolocation.co.jp>
2024-06-10 11:07:10 +0900

    add im-select and change scrolloff

--- init.lua
+++ init.lua
@@ -244,18 +244,6 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --
-  {
-    'keaising/im-select.nvim',
-    config = function()
-      require('im_select').setup {
-        default_im_select = 'com.google.inputmethod.Japanese.Roman',
-      }
-    end,
-  },
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
   -- for Arm64 Mac :: curl -Ls -o $HOME/bin/im-select https://raw.githubusercontent.com/daipeihust/im-select/master/macOS/out/apple/im-select
   -- for Intel Mac :: curl -Ls -o $HOME/bin/im-select https://raw.githubusercontent.com/daipeihust/im-select/master/macOS/out/intel/im-select
   {
