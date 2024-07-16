5e6f5b519f0344fafe1baed9f6141e5e07b2a4a7
Vladislav Grechannik <52157081+VlaDexa@users.noreply.github.com>
2024-07-16 18:05:40 +0200

    which-key v3 update (#1022)
    * which-key v3 update
    
    * remove unneeded brackets from which-key registration

--- init.lua
+++ init.lua
@@ -344,6 +344,16 @@ require('lazy').setup({
         { 'gr', group = 'LSP Actions', mode = { 'n' } },
       },
     },
+--      require('which-key').add {
+--        { '<leader>c', group = '[C]ode' },
+--        { '<leader>d', group = '[D]ocument' },
+--        { '<leader>r', group = '[R]ename' },
+--        { '<leader>s', group = '[S]earch' },
+--        { '<leader>w', group = '[W]orkspace' },
+--        { '<leader>t', group = '[T]oggle' },
+--        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+--      }
+--    end,
   },
 
   -- NOTE: Plugins can specify dependencies.
