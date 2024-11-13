c3c2f17c28838845a1369f9c274cb081c284fb37
Yamada Kenji <kenji.yamada@gmail.com>
2024-11-13 19:56:33 +0900

    update: disable tsserver

--- init.lua
+++ init.lua
@@ -657,7 +657,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        -- tsserver = {},
         --
 
         stylua = {}, -- Used to format Lua code
