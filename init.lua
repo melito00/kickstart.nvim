a4df66a1497848ad77e7f4e86aa8d0836a5d6751
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-03-12 17:24:44 -0400

    feat: add `vim.opt.confirm = true` (#1384)

--- init.lua
+++ init.lua
@@ -168,6 +168,11 @@ vim.o.cursorline = true
 vim.o.confirm = true
 vim.opt.scrolloff = 5 -- default 0, kickstart set 10
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
